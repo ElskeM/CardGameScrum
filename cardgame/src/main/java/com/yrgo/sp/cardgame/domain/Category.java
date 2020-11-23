8c4bd147c16ee7b1dc3e635bf776f168d99d688a
ElskeM <e.muijderman@gmail.com>
2020-11-23 12:15:58 +0100

    Första setup av Player och Category

--- cardgame/src/main/java/com/yrgo/sp/cardgame/domain/Category.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/domain/Category.java
@@ -1,5 +1,40 @@
 package com.yrgo.sp.cardgame.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String category;
+
+	public Category() {
+	}
+
+
+	public Category(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [category=" + category + "]";
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
--- cardgame/src/main/java/com/yrgo/sp/cardgame/domain/Player.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/domain/Player.java
@@ -1,5 +1,61 @@
 package com.yrgo.sp.cardgame.domain;
 
-public class Player {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Player {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String userName;
+	private String email;
+	private String password;
+	
+	public Player() {}
+	
+	public Player(String userName, String email, String password) {
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", userName=" + userName + ", email=" + email + "]";
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	
 }
