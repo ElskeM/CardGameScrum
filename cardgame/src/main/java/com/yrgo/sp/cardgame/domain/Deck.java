9c60699c461e268319d053d9274a302d9c155124
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2020-11-24 11:15:03 +0100

    first implementation of Deck class

--- cardgame/src/main/java/com/yrgo/sp/cardgame/domain/Deck.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/domain/Deck.java
@@ -1,5 +1,143 @@
 package com.yrgo.sp.cardgame.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ * 
+ * @author ptemrz
+ *
+ */
+@Entity
 public class Deck {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	/**
+	 * The name of this deck
+	 */
+	@Column(unique = true, nullable = false)
+	private String name;
+
+	/**
+	 * Who created this deck? Default: "Admin"
+	 */
+	@Column(columnDefinition = "varchar(255) default 'Admin'")
+	private String creator;
+
+	/**
+	 * Is this deck publicly visible, or only for the creator?
+	 */
+	private boolean isPublic;
+
+	/**
+	 * One card can belong to many different decks and one deck can have many
+	 * different cards Duplicates not allowed (should they be?)
+	 */
+	@ManyToMany
+	private Set<Card> cards;
+
+	@ManyToMany
+	private Set<Category> categories;
+
+	public Deck() {
+	}
+
+	/**
+	 * Create a deck builder instead?
+	 */
+	public Deck(String name, String creator, Set<Card> cards) {
+		super();
+		this.name = name;
+		this.creator = creator;
+		this.isPublic = false;
+		if (cards == null) {
+			cards = new HashSet<Card>();
+		}
+		this.cards = cards;
+		autoComputeCategories();
+	}
+
+	private void autoComputeCategories() {
+		this.categories = this.cards.stream().map(c -> c.getCategory()).collect(Collectors.toSet());
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public boolean isPublic() {
+		return isPublic;
+	}
+
+	public void setPublic(boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+
+	public Set<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(Set<Card> cards) {
+		this.cards = cards;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
+	}
+
+	public void addCard(Card c) {
+		if (this.cards.add(c)) {
+			autoComputeCategories();
+		}
+	}
+
+	public void removeCard(Card c) {
+		if (this.cards.remove(c)) {
+			autoComputeCategories();
+		}
+	}
+	
+	public List<Card> getShuffled(){
+		List<Card> shuffled = new ArrayList<Card>(this.cards);
+		Collections.shuffle(shuffled);
+		return shuffled;
+	}
 }
