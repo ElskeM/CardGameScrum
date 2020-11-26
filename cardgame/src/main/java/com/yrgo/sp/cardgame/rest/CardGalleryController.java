64bc23ee1e5c6c31609cb18b4d46f336164291a4
Bollpiska <bollpiska@hotmail.com>
2020-11-26 10:21:27 +0100

    lade till cross-origin annotation till allCards()

--- cardgame/src/main/java/com/yrgo/sp/cardgame/rest/CardGalleryController.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/rest/CardGalleryController.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.yrgo.sp.cardgame.data.CardRepository;
@@ -16,7 +17,7 @@ public class CardGalleryController {
 	@Autowired
 	private CardRepository cardData;
 	
-	
+	@CrossOrigin(origins = "http://localhost:8081")
 	@GetMapping("/allCards")
 	public CardList allCards() {
 		Card card1 = new Card("Klippa gräs", 1000);
