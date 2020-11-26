63b1511730e135eb1de70696fb77b1149bbbed1b
Bollpiska <bollpiska@hotmail.com>
2020-11-26 13:48:02 +0100

    lade till Game.vue

---
+++ cardgame_frontend/src/views/Game.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="number">{{this.number}}</div>
+        <div class="number">{{this.playerNumber}}</div>
+        <form action="post">
+            <input type="text" name="play-num">
+            <input type="submit" value="btn">
+        </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            number: 10,
+            playerNumber: ''
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
