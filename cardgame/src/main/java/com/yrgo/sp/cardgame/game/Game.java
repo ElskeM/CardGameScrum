86561e729cf90c301de7fb101c46f8df32c35cd5
Bollpiska <bollpiska@hotmail.com>
2020-11-26 11:41:30 +0100

    skapade Game-klass

---
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/game/Game.java
@@ -0,0 +1,30 @@
+package com.yrgo.sp.cardgame.game;
+
+import java.util.UUID;
+
+public class Game {
+	
+	private String id;
+	private int number = 10;
+	private int userNumber;
+	
+	public Game(String id) {
+	//	this.id = UUID.randomUUID().toString();
+		this.id = id;
+	}
+	
+	public String whoWins(int number, int userNumber) {
+		if(number > userNumber) {
+			return "YOU LOSE!";
+		} else {
+			return "YOU WIN!";
+		}
+	}
+	
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+	
+	
+
+}
