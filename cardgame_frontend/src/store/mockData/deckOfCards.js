8d832e4b69bba2a37ab52e70141557443b2eae2d
Bollpiska <bollpiska@hotmail.com>
2020-11-26 11:01:38 +0100

    store hämtar nu korttadata ifrån apiet

---
+++ cardgame_frontend/src/store/mockData/deckOfCards.js
@@ -0,0 +1,82 @@
+const deck = [
+       
+    {
+        id: 1,
+        title: "Kort 1",
+        description: "Ett jättebra kort",
+        score: 1000,
+        category: "Djur och natur"
+    },
+    {
+        id: 2,
+        title: "Kort 2",
+        description: "Ett dåligt kort",
+        score: 500,
+        category: "Djur och natur"
+    },
+    {
+        id: 3,
+        title: "Kort 3",
+        description: "Ett hyfsat kort",
+        score: 750,
+        category: "Djur och natur"
+    },
+    {
+        id: 4,
+        title: "Kort 4",
+        description: "Ett ganska dålig kort",
+        score: 100,
+        category: "Samhälle"
+    },
+    {
+        id: 5,
+        title: "Kort 5",
+        description: "Ett ganska bra kort",
+        score: 800,
+        category: "Samhälle"
+    },
+    {
+        id: 6,
+        title: "Kort 6",
+        description: "Ett mediokert kort",
+        score: 250,
+        category: "Samhälle"
+    },
+    {
+        id: 7,
+        title: "Kort 7",
+        description: "Ett jävla superkort",
+        score: 5000,
+        category: "Samhälle"
+    },
+    {
+        id: 8,
+        title: "Kort 8",
+        description: "Ett uselt kort",
+        score: 150,
+        category: "Historia"
+    },
+    {
+        id: 10,
+        title: "Kort 10",
+        description: "Ett rätt bra kort",
+        score: 550,
+        category: "Historia"
+    },
+    {
+        id: 11,
+        title: "Kort 11",
+        description: "Ett rakt av miserabelt kort",
+        score: 50,
+        category: "Historia"
+    },
+    {
+        id: 12,
+        title: "Kort 12",
+        description: "Ett kvalitetskort",
+        score: 2000,
+        category: "Människan"
+    } 
+    ]
+
+    export default deck
--- cardgame_frontend/src/store/modules/cardCollections.js
+++ cardgame_frontend/src/store/modules/cardCollections.js
@@ -1,85 +1,14 @@
 import axios from 'axios'
+//import deck from '../mockData/deckOfCards.js'
 
 
 const state = {
-    fullDeck: [{
-        id: 1,
-        title: "Kort 1",
-        description: "Ett jättebra kort",
-        score: 1000,
-        category: "Djur och natur"
-    },
-    {
-        id: 2,
-        title: "Kort 2",
-        description: "Ett dåligt kort",
-        score: 500,
-        category: "Djur och natur"
-    },
-    {
-        id: 3,
-        title: "Kort 3",
-        description: "Ett hyfsat kort",
-        score: 750,
-        category: "Djur och natur"
-    },
-    {
-        id: 4,
-        title: "Kort 4",
-        description: "Ett ganska dålig kort",
-        score: 100,
-        category: "Samhälle"
-    },
-    {
-        id: 5,
-        title: "Kort 5",
-        description: "Ett ganska bra kort",
-        score: 800,
-        category: "Samhälle"
-    },
-    {
-        id: 6,
-        title: "Kort 6",
-        description: "Ett mediokert kort",
-        score: 250,
-        category: "Samhälle"
-    },
-    {
-        id: 7,
-        title: "Kort 7",
-        description: "Ett jävla superkort",
-        score: 5000,
-        category: "Samhälle"
-    },
-    {
-        id: 8,
-        title: "Kort 8",
-        description: "Ett uselt kort",
-        score: 150,
-        category: "Historia"
-    },
-    {
-        id: 10,
-        title: "Kort 10",
-        description: "Ett rätt bra kort",
-        score: 550,
-        category: "Historia"
-    },
-    {
-        id: 11,
-        title: "Kort 11",
-        description: "Ett rakt av miserabelt kort",
-        score: 50,
-        category: "Historia"
-    },
-    {
-        id: 12,
-        title: "Kort 12",
-        description: "Ett kvalitetskort",
-        score: 2000,
-        category: "Människan"
-    } 
-    ]
+    fullDeck: 
+    
+    
+    [
+    ] 
+    
 }
 
 const getters = {
@@ -89,15 +18,23 @@ const getters = {
 }
 
 const actions = {
-    async fetchFullDeck() {
+
+
+    async fetchFullDeck({commit}) {
         const response = await axios.get('http://localhost:8080/allCards')
-        console.log(response.data)
+       commit('fillFullDeck', response.data.cards)
+         console.log(response.data) 
+
+
     }
 
 
 }
 
 const mutations = {
+    fillFullDeck(state, deck) {
+        state.fullDeck = deck
+    }
 
 }
 
