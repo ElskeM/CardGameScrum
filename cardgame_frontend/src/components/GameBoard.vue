1e9b1718b17e8233c0135f7c8acab534c2c75c1d
Simon Hagelin <simon_hagelin@hotmail.com>
2020-12-03 09:32:03 +0100

    before merge

---
+++ cardgame_frontend/src/components/GameBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+  <li v-for="card in playedCards" :value="card" :key="card.id">
+    {{ card.frontImage }}
+  </li>
+  </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      playedCards: []
+    };
+  }
+};
+</script>
