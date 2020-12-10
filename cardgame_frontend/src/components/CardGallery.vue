3b88317f35d6dc01b48f005925e061c18de42673
Bollpiska <bollpiska@hotmail.com>
2020-12-10 12:35:31 +0100

    nu används riktiga bilder i ĆardGallery

--- cardgame_frontend/src/components/CardGallery.vue
+++ cardgame_frontend/src/components/CardGallery.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-        
-        <div v-for="card in wholeCollection" :key="card.id">
-            
-            <DisplayCard v-bind:card="card"/>
+        <div class="gallery">
+       <!-- <div class="card" v-for="card in wholeCollection" :key="card.id">-->
+                <DisplayCard v-for="card in wholeCollection" :key="card.id" v-bind:card="card"/>
+           
+    <!--    </div> -->
         </div>
     </div>
     
@@ -35,4 +36,17 @@ export default {
 
 <style scoped>
 
+
+.gallery {
+    width: 100%;
+    height: 100%;
+    display:flex;
+    flex-wrap: wrap;
+    border: solid black 5px;
+    justify-content: center;
+}
+
+.card {
+    margin: 10px;
+}
 </style>
--- cardgame_frontend/src/components/DisplayCard.vue
+++ cardgame_frontend/src/components/DisplayCard.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
         <div class="container">
-            <h3>{{this.card.title}}</h3> 
+       
+
+        <router-link :to="{ path: `/singlecard/${this.card.id}`}" class="card">
+            <img
+            class="card"
+            :src="card.frontImage"
+            v-bind:href="this.card.frontImage"
+          >
+        </router-link>
+        
+    <!--        <h3>{{this.card.title}}</h3> 
             <h4>{{this.card.subtitle}}</h4>
             <div>{{this.card.description}}</div>
             <div>{{this.card.score}} kg</div>
-            <div><a v-bind:href="this.card.frontImage">Card Front</a></div>
+            <div>
+                <a v-bind:href="this.card.frontImage">Card Front</a>
+            </div> -->
 
         </div>
     </div>
@@ -21,10 +33,25 @@ export default {
 <style scoped>
 
 .container {
-    border: 2px solid red;
+    
     width: 150px;
     height: 250px;
-    background: grey;
+    
 
+}
+
+
+img {
+    width: 150px;
+    height: 250px
+}
+
+.card {
+    transition: transform .2s;
+    border-radius: 1rem;
+}
+
+.card:hover {
+    transform: scale(1.15)
 }
 </style>
