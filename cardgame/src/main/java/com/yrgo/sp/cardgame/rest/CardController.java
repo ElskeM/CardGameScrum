5d3b7a18c6c126166248304dea55d4ee6cc35578
ElskeM <e.muijderman@gmail.com>
2020-12-14 11:25:30 +0100

    korrigerade fel i cross origins

--- cardgame/src/main/java/com/yrgo/sp/cardgame/rest/CardController.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/rest/CardController.java
@@ -32,7 +32,7 @@ import com.yrgo.sp.cardgame.domain.Category;
 import com.yrgo.sp.exception.CardNotFoundException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:")
+@CrossOrigin(origins = "http://localhost:8081")
 public class CardController {
 
 	private static final Logger LOG = LogManager.getLogger(CardController.class);
