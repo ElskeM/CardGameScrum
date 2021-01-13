c6cbfb090a74f98f77febd12340ed28b982eb196
ElskeM <e.muijderman@gmail.com>
2021-01-13 14:15:46 +0100

    fixade två små buggar: duplicate namn i router och return model false i bigcardinfo så att rätt sidan visas när man trycker på ett kort

--- cardgame_frontend/src/router/index.js
+++ cardgame_frontend/src/router/index.js
@@ -34,7 +34,7 @@ const routes = [
   },
   {
     path: '/game/:id',
-    name: 'Game',
+    name: 'GameId',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
