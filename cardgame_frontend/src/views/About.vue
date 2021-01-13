7a98f5bb7959ddebc7fcf6199dd21c80c4f6d09f
ElskeM <e.muijderman@gmail.com>
2021-01-13 10:45:57 +0100

    ändrade about sidan

--- cardgame_frontend/src/views/About.vue
+++ cardgame_frontend/src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Om Klimatkoll</h1>
+    <div>
+    <p>Det här online spel är baserat på kortspelet Klimatkoll, som ges ut av <strong>Klimatkoll Guldheden AB</strong>.</p> 
+    <p>Online versionen är skapad med syftet att göra spelet tillgängling till ett större publik.</p>
+    <p>Är du intresserad och vill du veta mer om kortspelet Klimatkoll eller köpa en fysik version? 
+      Då är du varmt välkommen på Klimatkolls officiella <a href="https://www.kortspeletklimatkoll.se/" target="_blank">websidan</a>.</p> 
+    </div>
   </div>
 </template>
