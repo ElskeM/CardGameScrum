82a6d1c625e16c80789e5e21c218e1435072e77f
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2021-01-14 09:17:19 +0100

    correct spelling in About.vue

--- cardgame_frontend/src/views/About.vue
+++ cardgame_frontend/src/views/About.vue
@@ -2,10 +2,10 @@
   <div class="about">
     <h1>Om Klimatkoll</h1>
     <div>
-    <p>Det här online spel är baserat på kortspelet Klimatkoll, som ges ut av <strong>Klimatkoll Guldheden AB</strong>.</p> 
-    <p>Online versionen är skapad med syftet att göra spelet tillgängling till ett större publik.</p>
-    <p>Är du intresserad och vill du veta mer om kortspelet Klimatkoll eller köpa en fysik version? 
-      Då är du varmt välkommen på Klimatkolls officiella <a href="https://www.kortspeletklimatkoll.se/" target="_blank">websidan</a>.</p> 
+    <p>Det här onlinespelet är baserat på kortspelet Klimatkoll, som ges ut av <strong>Klimatkoll Guldheden AB</strong>.</p> 
+    <p>Online versionen är skapad med syftet att göra spelet tillgängling till en större publik.</p>
+    <p>Är du intresserad och vill veta mer om kortspelet Klimatkoll eller köpa en fysisk version? 
+      Då är du varmt välkommen till Klimatkolls officiella <a href="https://www.kortspeletklimatkoll.se/" target="_blank">webbsida</a>.</p> 
     </div>
   </div>
 </template>
