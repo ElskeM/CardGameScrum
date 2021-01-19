b75bbcde89e4f26c430f07cff0cfb288b7670cd8
ElskeM <e.muijderman@gmail.com>
2021-01-19 11:27:20 +0100

    lite styling för about sidan

--- cardgame_frontend/src/views/About.vue
+++ cardgame_frontend/src/views/About.vue
@@ -1,11 +1,38 @@
 <template>
-  <div class="about">
-    <h1>Om Klimatkoll</h1>
-    <div>
-    <p>Det här onlinespelet är baserat på kortspelet Klimatkoll, som ges ut av <strong>Klimatkoll Guldheden AB</strong>.</p> 
-    <p>Online versionen är skapad med syftet att göra spelet tillgängling till en större publik.</p>
-    <p>Är du intresserad och vill veta mer om kortspelet Klimatkoll eller köpa en fysisk version? 
-      Då är du varmt välkommen till Klimatkolls officiella <a href="https://www.kortspeletklimatkoll.se/" target="_blank">webbsida</a>.</p> 
+  <div class="overall">
+    <div class="about">
+      <h2>Om Klimatkoll</h2>
+      <div>
+        <p>
+          Det här onlinespelet är baserat på kortspelet Klimatkoll, som ges ut
+          av
+          <strong>Klimatkoll Guldheden AB</strong>.
+        </p>
+        <p>
+          Online versionen är skapad med syftet att göra spelet tillgängling
+          till en större publik.
+        </p>
+        <p>
+          Är du intresserad och vill veta mer om kortspelet Klimatkoll eller
+          köpa en fysisk version?
+        </p>
+        <p>
+          Då är du varmt välkommen till Klimatkolls officiella
+          <a href="https://www.kortspeletklimatkoll.se/" target="_blank"
+            >webbsida</a
+          >.
+        </p>
+      </div>
     </div>
   </div>
 </template>
+<style scoped>
+.overall {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+}
+.about {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
