9fd32b2f1b2b27c092533886ddec28f0b885de6a
ElskeM <e.muijderman@gmail.com>
2021-01-27 10:36:03 +0100

    javadoc comments

--- cardgame/src/main/java/com/yrgo/sp/cardgame/chat/ChatWSController.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/chat/ChatWSController.java
@@ -4,10 +4,20 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+/**
+ * @author pontus
+ * ChatWSController entity which takes care of the chatmessage mapping for the client project 
+ *
+ */
 @Controller
 public class ChatWSController {
 
 	
+	/**
+	 * Method that returns a new chatmessage to a chat
+	 * @param message
+	 * @return message
+	 */
 	@MessageMapping("/chatmessage/{id}")
 	@SendTo("/cardgame/chat/{id}")
 	public ChatMessage newChatMessage(ChatMessage message) {
