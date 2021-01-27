8d82ced071c7064052774303ebf0b6683d7a6eb0
ElskeM <e.muijderman@gmail.com>
2021-01-27 21:57:45 +0100

    mer javadoc

--- cardgame/src/main/java/com/yrgo/sp/cardgame/game/KlimatkollListener.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/game/KlimatkollListener.java
@@ -1,7 +1,29 @@
 package com.yrgo.sp.cardgame.game;
 
+/**
+ * @author simon
+ * Interface that functions as a listener in the game
+ */
 public interface KlimatkollListener {
+	
+	/**
+	 * Method to keep track if the game is draw
+	 * @param game
+	 */
 	public void gameIsDraw(Game g);
+	
+	
+	/**
+	 * method to keep track of the timer 
+	 * @param game
+	 */
 	public void timerRunOut(Game g);
+	
+	
+	/**
+	 * method that keeps track of the timer, if it runs out 3 times, the player loses.
+	 * @param game
+	 * @param player
+	 */
 	public void walkover(Game g, Player p);
 }
