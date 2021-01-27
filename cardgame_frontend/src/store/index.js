a675ef710dfb7ac49dc5ded72e64952a041e5fa8
Peter Mrazek <56875892+ptemrz@users.noreply.github.com>
2021-01-27 14:16:34 +0100

    clean up store

--- cardgame_frontend/src/store/index.js
+++ cardgame_frontend/src/store/index.js
@@ -1,18 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import cardCollections from './modules/cardCollections'
-import userData from './modules/userData'
-//import createMultiTabState from 'vuex-multi-tab-state';
+import Vue from "vue";
+import Vuex from "vuex";
+import cardCollections from "./modules/cardCollections";
+import userData from "./modules/userData";
 
-
-
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     cardCollections,
     userData,
- //   plugins: [createMultiTabState()]
-
-}
-})
+  },
+});
