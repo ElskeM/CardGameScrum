b92e95995a4c3c75fdffe2bc64cb5cfe1a8a479f
ElskeM <e.muijderman@gmail.com>
2021-01-28 13:34:55 +0100

    lade till javadoc på inactivitylistener

--- cardgame/src/main/java/com/yrgo/sp/cardgame/game/InactivityListener.java
+++ cardgame/src/main/java/com/yrgo/sp/cardgame/game/InactivityListener.java
@@ -1,5 +1,13 @@
 package com.yrgo.sp.cardgame.game;
 
+/**
+ * @author ptemrz
+ * InactivityListener interface to keep track of the question if the game has gone inactive
+ */
 public interface InactivityListener {
+	/**
+	 * keeps track of inactive games, so their data can be removed from the server
+	 * @param gameId
+	 */
 	void hasGoneInactive(long gameId);
 }
